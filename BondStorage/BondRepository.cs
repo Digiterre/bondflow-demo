2b08980cb3dd06e018857fe873354f100f6f54a1
simonH <simon@slimtech-it.co.uk>
2016-06-02 15:02:07 +0100

    Bond Storage (initially to a file)

---
+++ BondStorage/BondRepository.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using EnergyTrading.Xml.Serialization;
+using Microsoft.Practices.EnterpriseLibrary.Common.Configuration.Design;
+using Model;
+
+namespace BondStorage
+{
+    public class BondRepository
+    {
+        private static List<Bond> bonds = null;
+        private static string fileName = "Bonds.xml";
+
+        private static void LoadBonds()
+        {
+            if (bonds == null)
+            {
+                if (File.Exists(fileName))
+                {
+                    var source = File.ReadAllText(fileName);
+                    bonds = source.DeserializeDataContractXmlString<List<Bond>>();
+                }
+                else
+                {
+                    bonds = new List<Bond>();
+                }
+            }
+        }
+
+        private static void SaveBonds()
+        {
+            File.WriteAllText(fileName, bonds.DataContractSerialize());
+        }
+
+        public static void Add(Bond bond)
+        {
+            LoadBonds();
+            bonds.Add(bond);
+            SaveBonds();
+        }
+
+
+    }
+}
---
+++ BondStorage/StorageMessageHandler.cs
@@ -0,0 +1,13 @@
+﻿using Messaging.Transport;
+using Model;
+
+namespace BondStorage
+{
+    public class StorageMessageHandler : IHandleIncomingMessages<Bond>
+    {
+        public void Handle(Bond bond)
+        {
+            BondRepository.Add(bond);
+        }
+    }
+}
