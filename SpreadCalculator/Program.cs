fd898c763ad429097961e8790e77fc4a000d383e
simonH <simon@slimtech-it.co.uk>
2016-06-02 14:26:05 +0100

    Fixed percentage spread calculator

---
+++ SpreadCalculator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnergyTrading.Configuration;
+using EnergyTrading.Container.Unity;
+using Messaging.Unity.Configuration;
+using TransportMetadata.Registrars;
+
+namespace SpreadCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configurator = new TransportConfigurator()
+                                   .ConfigureLogging()
+                                   .ConfigureContainer()
+                                   .Register<MetadataMapperRegistrar>()
+                                   .StandardRegistrars()
+                                   .ConfigureRegistrars();
+
+            // Any other start up tasks we want.
+            ConfigurationBootStrapper.Initialize(configurator.Container.ResolveAllToEnumerable<IGlobalConfigurationTask>());
+
+            // Get and start the publisher
+            var receivingManager = configurator.AsReceivingManager();
+            receivingManager.Start();
+            Console.WriteLine("Enter quit to exit");
+            while (true)
+            {
+                var entered = Console.ReadLine();
+                if (entered == "quit")
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ SpreadCalculator/SpreadCalculatorMessageHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Messaging.Transport;
+using Model;
+
+namespace SpreadCalculator
+{
+    public class SpreadCalculatorMessageHandler : IHandleIncomingMessages<Bond>
+    {
+        private readonly ITransport transport;
+
+        public SpreadCalculatorMessageHandler(ITransport transport)
+        {
+            if (transport == null)
+            {
+                throw new ArgumentNullException(nameof(transport));
+            }
+            this.transport = transport;
+        }
+
+        public void Handle(Bond bond)
+        {
+            if (bond.MidPrice.HasValue)
+            {
+                var difference = bond.MidPrice.Value*0.015m;
+                bond.Spread = new PriceSpread {Lower = bond.MidPrice - difference, Upper = bond.MidPrice + difference};
+            }
+            transport.Send(bond);
+        }
+    }
+}
