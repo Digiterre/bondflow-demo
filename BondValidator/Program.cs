b413d6b49524c9310af799781b84811d0fff798f
simonH <simon@slimtech-it.co.uk>
2016-06-02 14:14:14 +0100

    Bond Validator process Bond must have Name and Price. (no error handling)

---
+++ BondValidator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using EnergyTrading.Configuration;
+using EnergyTrading.Container.Unity;
+using Messaging.Unity.Configuration;
+using TransportMetadata.Registrars;
+
+namespace BondValidator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configurator = new TransportConfigurator()
+                                   .ConfigureLogging()
+                                   .ConfigureContainer()
+                                   .Register<MetadataMapperRegistrar>()
+                                   .StandardRegistrars()
+                                   .ConfigureRegistrars();
+
+            // Any other start up tasks we want.
+            ConfigurationBootStrapper.Initialize(configurator.Container.ResolveAllToEnumerable<IGlobalConfigurationTask>());
+
+            // Get and start the publisher
+            var receivingManager = configurator.AsReceivingManager();
+            receivingManager.Start();
+            Console.WriteLine("Enter quit to exit");
+            while (true)
+            {
+                var entered = Console.ReadLine();
+                if (entered == "quit")
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ BondValidator/ValidationMessageHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Messaging.Transport;
+using Microsoft.Practices.ObjectBuilder2;
+using Model;
+
+namespace BondValidator
+{
+    public class ValidationMessageHandler : IHandleIncomingMessages<Bond>
+    {
+        private readonly ITransport transport;
+        public ValidationMessageHandler(ITransport transport)
+        {
+            if (transport == null)
+            {
+                throw new ArgumentNullException(nameof(transport));
+            }
+            this.transport = transport;
+        }
+
+        public void Handle(Bond bond)
+        {
+            if (string.IsNullOrWhiteSpace(bond.Name) || !bond.MidPrice.HasValue)
+            {
+                // error case : really should do something here
+            }
+            else
+            {
+                transport.Send(bond);
+            }
+        }
+    }
+}
